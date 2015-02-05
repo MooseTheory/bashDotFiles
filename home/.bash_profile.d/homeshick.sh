5421421f541bf65d6e0dfac8a209479cd4dd956d
MooseTheory <me@davidmerris.com>
2015-02-05 14:43:26 -0500

    Added Autocomplete To Homeshick

--- home/.bash_profile.d/homeshick.sh
+++ home/.bash_profile.d/homeshick.sh
@@ -1,2 +1,5 @@
-#alias homesick="$HOME/.homesick/repos//homeshick/home/.homeshick"
+# Include homeshick.
 source $HOME/.homesick/repos/homeshick/homeshick.sh
+
+# Enable homeshick autocompletion
+source $HOME/.homesick/repos/homeshick/completions/homeshick-completion.bash
